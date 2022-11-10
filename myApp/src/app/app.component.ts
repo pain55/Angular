bfeddfdcde4722bff0e83691807c3ae6734981f2
Sai <saikiranmsk34@gmail.com>
2022-11-10 09:38:57 +0530

    Interpolation Tutorial

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html' ,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'myApp';
+  name: String = "Sai";
+  flag: boolean = false;
+
+  greet() : String{
+    return "hello " + this.name;
+  }
 }
